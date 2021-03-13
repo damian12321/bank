128a1c1a32ab60d650cfa8da9f57480e2287ddd7
Damian Juruś <damianjurus@wp.pl>
2021-03-13 19:55:39 +0100

    Updated security

---
+++ src/main/java/pl/bank/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package pl.bank.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
